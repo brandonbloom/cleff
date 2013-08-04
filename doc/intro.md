fbfc42d8c93cf1ed42b53168aec2f0fb163b8ec7
Brandon Bloom <brandon@brandonbloom.name>
2013-08-04 10:35:08 -0700

    lein new cleff

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cleff
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cleff/core_test.clj
@@ -0,0 +1,7 @@
+(ns cleff.core-test
+  (:require [clojure.test :refer :all]
+            [cleff.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
