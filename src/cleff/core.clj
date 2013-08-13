19f776f7d32247b7244431e349d3f91a06e4c865
Brandon Bloom <brandon@brandonbloom.name>
2013-08-13 18:11:47 -0400

    Kinda sorta working almost!

--- src/cleff/core.clj
+++ src/cleff/core.clj
@@ -1,5 +1,6 @@
 (ns cleff.core
-  (:require [clojure.core.async.impl.ioc-macros :as ioc])
+  (:require [clojure.core.async.impl.ioc-macros :as ioc
+             :refer (state-machine run-state-machine aget-object aset-all!)])
   (:import [java.util.concurrent.atomic AtomicReferenceArray]))
 
 
@@ -9,32 +10,57 @@
   (let [n (.length state)
         clone (AtomicReferenceArray. n)]
     (doseq [i (range n)]
-      (ioc/aset-object clone i (ioc/aget-object state i)))
+      (aset-all! clone i (aget-object state i)))
     clone))
 
 
 ;;; Terminator implementations
 
-(def handler-idx ioc/USER-START-IDX)
-(def continuation-idx ioc/USER-START-IDX)
+(def trampoline-idx ioc/USER-START-IDX)
+(def communication-idx (+ trampoline-idx 1))
 
-(defn handler-message []
-  )
+(defn handler-message [state blk]
+  (println "handler-message")
+  (aset-all! state ioc/STATE-IDX blk)
+  nil)
 
-(defn handler-continue []
-  )
+(defn handler-continue [state blk value]
+  (println "handler-continue")
+  (aset-all! state
+             ioc/STATE-IDX blk
+             ioc/VALUE-IDX value
+             trampoline-idx :continue)
+  nil)
 
-(defn handler-return []
-  )
+(defn handler-return [state value]
+  (println "handler-return")
+  (aset-all! state
+             ioc/VALUE-IDX value
+             trampoline-idx :handled)
+  nil)
 
-(defn computation-effect []
-  )
+(defn computation-effect [state blk & message]
+  (println "computation-effect")
+  (aset-all! state
+             ioc/STATE-IDX blk
+             trampoline-idx :effect
+             communication-idx message)
+  nil)
 
-(defn computation-value []
-  )
+(defn computation-value [state blk value]
+  (println "computation-value")
+  (aset-all! state
+             ioc/STATE-IDX blk
+             trampoline-idx :value
+             communication-idx value)
+  nil)
 
-(defn computation-return []
-  )
+(defn computation-return [state value]
+  (println "computation-return")
+  (aset-all! state
+             ioc/VALUE-IDX value
+             trampoline-idx :computed)
+  nil)
 
 
 ;;; Coroutine state machines
@@ -51,31 +77,77 @@
 
 (defn handler-form [operations]
   (let [args-sym (gensym "args__")]
-    `(let [[operation# ~args-sym] (~'message)]
+    `(let [[operation# & ~args-sym] (~'message)]
        (case operation#
-         ~@(concat (for [[operation arglist body] operations]
-                     `[operation (let [~arglist ~args-sym]
-                                   ~@body)]))
+         ~@(apply concat (for [[operation arglist & body] operations]
+                           `[~operation (let [~arglist ~args-sym]
+                                          ~@body)]))
          ;;TODO forward operation up the handler stack
          (throw (Exception. (str "Unexpected operation " operation#)))))))
 
 (defn handler-fn [bindings-sym env operations]
-  `(let [state# (~(ioc/state-machine (list (handler-form operations))
-                                     0 env handler-terminators))]
-     (ioc/aset-object state# ioc/BINDINGS-IDX ~bindings-sym)
-     (ioc/run-state-machine state#)
+  `(let [state# (~(state-machine (list (handler-form operations))
+                                 2 env handler-terminators))]
+     (aset-all! state# ioc/BINDINGS-IDX ~bindings-sym)
+     (run-state-machine state#)
      state#))
 
 (defn computation-fn [bindings-sym env body]
-  `(let [state# (~(ioc/state-machine body 0 env computation-terminators))]
-     (ioc/aset-object state# ioc/BINDINGS-IDX ~bindings-sym)))
+  `(let [state# (~(state-machine body 2 env computation-terminators))]
+     (aset-all! state# ioc/BINDINGS-IDX ~bindings-sym)))
 
 
 ;;; Interpretation trampoline
 
 (defn run [handler computation]
   (loop [stack (list computation)]
-    ))
+    (println "loop depth:" (count stack))
+    (let [frame (peek stack)]
+      (run-state-machine frame)
+      (case (aget-object frame trampoline-idx)
+        :effect
+          (let [message (aget-object frame communication-idx)
+                _ (println "Effect:" (pr-str message))
+                handler* (aset-all! (clone-state handler)
+                                    ioc/VALUE-IDX message
+                                    communication-idx frame)]
+            (recur (-> stack pop (conj handler*))))
+        :value
+          (let [value (aget-object frame communication-idx)
+                _ (println "Value:" (pr-str value))
+                handler* (aset-all! (clone-state handler)
+                                    ioc/VALUE-IDX value)]
+            (recur (-> stack pop (conj handler*))))
+        :continue
+          (let [value (aget-object frame ioc/VALUE-IDX)
+                _ (println "Continue:" (pr-str value))
+                continuation (aget-object frame communication-idx)
+                frame* (aset-all! (clone-state continuation)
+                                  ioc/VALUE-IDX value)]
+            (recur (conj stack frame*)))
+        :handled
+          (let [value (aget-object frame ioc/VALUE-IDX)
+                _ (println "Handled:" (pr-str value))
+                stack* (pop stack)
+                frame* (peek stack*)]
+            (if frame*
+              (do
+                (aset-all! frame* ioc/VALUE-IDX value)
+                (recur stack*))
+              value))
+        :computed
+          (let [value (aget-object frame ioc/VALUE-IDX)
+                _ (println "Computed:" (pr-str value))
+                stack* (pop stack)
+                frame* (peek stack*)]
+            (if frame*
+              (do
+                (aset-all! frame* ioc/VALUE-IDX value)
+                (recur stack*))
+              value))
+        (throw (Exception. (str "Unexpected trampoline command:  "
+                                (pr-str (aget-object frame trampoline-idx)))))
+        ))))
 
 (defn handle-fn [env operations computation-body]
   (let [bindings-sym (gensym "bindings__")]
@@ -104,16 +176,34 @@
     (-> form macroexpand ppc))
 
   (defmacro foo [& form]
-    (ioc/state-machine form 2 &env {:Return handler-return}))
+    (state-machine form 0 &env {:Return `handler-return}))
+
+  (ppme '(foo :bar))
 
   (ppme
     '(foo (let [[x y z] :bar] [z y x]))
     )
 
+
   (ppc (handler-form '[]))
+  (ppc (handler-form '[(decide [] (continue true))]))
 
   (handle []
-    1)
+    :foo)
+
+  (handle [(decide [] (continue true))]
+    (let [x (if (effect 'decide) 10 20)
+          y (if (effect 'decide) 0 5)]
+      (- x y)))
+  ;;=> 10
+
+  (handle [(decide [] (continue false))]
+    (let [x (if (effect 'decide) 10 20)
+          y (if (effect 'decide) 0 5)]
+      (- x y)))
+  ;;=> 15
+
+  ;; not working yet below here
 
   (handle [(decide [] (continue true))]
     (let [x (if (decide c) 10 20)
